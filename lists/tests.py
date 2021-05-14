6143ed52ee7c561c3764b5dfe6ba1f2a5a59b393
WK-Chen <k987309177@gmail.com>
2021-05-14 16:54:20 +0800

    update

--- lists/tests.py
+++ lists/tests.py
@@ -30,18 +30,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-    
-    
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        
-        response = self.client.get('/')
-        
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertEqual(response['location'], '/list/the-only-list-in-the-world/')
         
         
 class ItemModelTest(TestCase):
@@ -62,4 +51,13 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
-  
+
+class ListViewTest(TestCase):
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/list/the-only-list-in-the-world/')
+        
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
